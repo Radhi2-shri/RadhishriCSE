eb8ab6a1843bfcc301ec5ca7e99d79e11b14ddc3
Radhi2-shri <radhishri2004@gmail.com>
2024-10-15 10:30:28 +0530

    sowmi kundhani

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Radhishri</h1>
+    <h1>sowmi mayiru</h1>
   </React.StrictMode>
 );
 
